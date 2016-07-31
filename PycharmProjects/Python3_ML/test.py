f26549db0d6ff12e69db6c0805e9d009a8bd6814
kuatroka <aaarmani_2000@yahoo.com>
2016-07-31 17:13:15 +0800

    cv

---
+++ PycharmProjects/Python3_ML/test.py
@@ -0,0 +1,29 @@
+import numpy as np
+"""
+Fit training data.
+
+    Parameters
+    ----------
+    X : {array-like}, shape = [n_samples, n_features]
+        Training vectors, where n_samples is the number of samples and
+        n_features is the number of features.
+    y : array-like, shape = [n_samples]
+        Target values.
+
+    Returns
+    -------
+    self : object
+
+"""
+selfw_ = np.zeros((5,30), dtype=np.int)
+selfw_[3,3] = 3
+print(selfw_)
+selferrors_ = []
+print(selferrors_.append('36'))
+
+a = np.array([(0.1, 1.), (0.1, 2.), (0.1, 3.), (0.1, 4.), (0.1, 5.)])
+print(a)
+print(a.T)
+
+
+
