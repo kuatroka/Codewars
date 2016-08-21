e767dea9922c2a947d893725992c61f52391d7de
kuatroka <aaarmani_2000@yahoo.com>
2016-08-22 00:49:34 +0800

    ...

---
+++ PycharmProjects/Codewars/CW_fuel_price.py
@@ -0,0 +1,16 @@
+def fuelPrice(litres, pricePerLiter):
+    """
+    In this kata you will have to write a function called fuelPrice (fuel_price in PHP) that takes litres and
+    pricePerLiter as arguments. Purchases of 2 or more litres get a discount of 5 cents per litre, purchases
+    of 4 or more litres get a discount of 10 cents per litre, and so on every two litres, up to a maximum
+    discount of 25 cents per litre. But total discount per liter cannot be more than 25 cents. round answer to 2
+    decimal places. Also you can guess that there will not be negative or non-numeric inputs.
+    """
+
+
+
+
+
+# Test.assert_equals(fuelPrice(10, 21.5), 212.50);
+# Test.assert_equals(fuelPrice(40, 10), 390.00);
+# Test.assert_equals(fuelPrice(15, 5.83), 83.70);
